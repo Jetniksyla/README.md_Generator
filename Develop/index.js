1a19713a42b40ec02c15db6aeef8ca4ac5214c4e
Jetnik Syla <sjetnik@gmail.com>
2024-02-22 13:42:34 -0500

    fixing spaces in the readme

---
+++ Develop/index.js
@@ -0,0 +1,92 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("././utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of the project? (Required)",
+    validate: function (input) {
+      return input !== "" ? true : "Please enter a valid name.";
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description of the project:",
+    validate: function (input) {
+      return input !== ""
+        ? true
+        : "Please Provide a description of the project.";
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Does your project require installation instructions? If so, provide them below.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please choose your preferred license for this project: ",
+    choices: ["MIT", "Apache-2.0", "GPL-3.0", "BSD2", "BSD3", "None"],
+    validate: function (input) {
+      return input !== "" ? true : "Please choose your preferred license:";
+    },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide example on how to use your repo: ",
+  },
+  {
+    type: "input",
+    name: "required",
+    message: "What command should be run to install required dependencies? ",
+    default: "(npm i), add '@version' if needed",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "What is your GitHum username? ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message:
+      "How can other developers contribute to this project? Provide guidelines for how to do so: ",
+  },
+  {
+    type: "input",
+    name: "credit",
+    message:
+      "Please provide the names of those that need to credited for the development of the application.",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, "utf8", (error) =>
+    error
+      ? console.error(error)
+      : console.log("Successfully created README.md!")
+  );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    fs.writeFileSync("README.md", generateMarkdown({ ...answers }));
+  });
+}
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,72 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+
+// Function to render the license badge
+function renderLicenseBadge(license) {
+  if (license && license !== "none") {
+    // Generate badge based on the license provided
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`;
+  }
+  return "";
+}
+// TODO: Create a function that returns the license link
+
+// Function to render the license link
+function renderLicenseLink(license) {
+  if (license && license !== "none") {
+    return `[License](#license)`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license section of README
+
+// Function to render the license section of README
+function renderLicenseSection(license) {
+  if (license && license !== "none") {
+    return `## License
+This project is licensed under the ${license} license. Click the [link](https://opensource.org/licenses/${license}) for more details.`;
+  }
+  return "";
+}
+
+// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [License](#license)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## License
+${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Screenshot of the Project
+![Screenshot](${data.screenshot})
+
+## Questions
+For questions about the project:
+- GitHub Profile: [${data.creator}](https://github.com/${data.creator})
+- Email: [${data.email}](mailto:${data.email})
+`;
+}
+
+module.exports = generateMarkdown;
